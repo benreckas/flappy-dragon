85755e83506d34fed016398ec51feb865463509b
Ben Reckas <benreckas@gmail.com>
2022-01-11 08:48:54 -0800

    Implement player and basic velocity logic

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,59 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct State {
+    frame_time: f32,
     mode: GameMode,
+    player: Player,
+}
+
+struct Player {
+    velocity: f32,
+    x: i32,
+    y: i32,
 }
 
 impl State {
-    fn new(mode: GameMode) -> Self {
-        State { mode }
+    fn new() -> Self {
+        State {
+            frame_time: 0.0,
+            player: Player::new(5, 25),
+            mode: GameMode::Menu,
+        }
     }
 
-    fn play(&mut self, _ctx: &mut BTerm) {
-        // TODO: Stub play
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
-        // TODO: Stub restart
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
-        // TODO: Stub menu
         ctx.cls();
         ctx.print_centered(5, "Welcome to Flappy Dragon");
         ctx.print_centered(8, "(P) Play Game");
@@ -67,10 +99,39 @@ impl GameState for State {
     }
 }
 
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State::new(GameMode::Menu))
+    main_loop(context, State::new())
 }
